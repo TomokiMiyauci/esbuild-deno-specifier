4f672af2a6f767580e777b17ff864ef4cbb3aa8e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-05-15 13:38:08 +0900

    test(load_as_directory): add test case

--- src/cjs/load_as_directory.ts
+++ src/cjs/load_as_directory.ts
@@ -1,44 +1,48 @@
 import { join, readPackageJson } from "../../deps.ts";
-import { isObject } from "../utils.ts";
-import { mainFields, readFile } from "../context.ts";
+import { isObject, isString } from "../utils.ts";
+import { readFile } from "../context.ts";
 import { resolveBrowser } from "../browser.ts";
 import { loadAsFile } from "./load_file.ts";
 import { loadIndex } from "./load_index.ts";
-import type { LoadResult } from "./types.ts";
+import type { Context, LoadResult } from "./types.ts";
 
+/**
+ * @throws {Error}
+ */
 export async function loadAsDirectory(
-  M: URL,
+  packageURL: URL | string,
+  context: Pick<Context, "mainFields">,
 ): Promise<LoadResult | undefined | false> {
   // 1. If X/package.json is a file,
-  const pjson = await readPackageJson(M, { readFile });
+  const pjson = await readPackageJson(packageURL, { readFile });
 
   if (pjson) {
-    const fieldSet = mainFields.map((field) => ({ field, value: pjson[field] }))
-      .filter((v): v is { field: string; value: string } =>
-        typeof v.value === "string"
-      );
-    const browser = pjson.browser;
-    const isBrowser = isObject(browser);
+    const values = context.mainFields.map((field) => pjson[field]).filter(
+      isString,
+    );
+    // const browser = pjson.browser;
+    // const isBrowser = isObject(browser);
 
-    if (fieldSet.length) {
-      for (const { value } of fieldSet) {
-        let url: URL;
+    // b. If "main" is a falsy value, GOTO 2.
+    if (values.length) {
+      for (const value of values) {
+        const url = join(packageURL, value);
 
-        if (isBrowser) {
-          const result = resolveBrowser(value, browser);
+        // if (isBrowser) {
+        //   const result = resolveBrowser(value, browser);
 
-          if (result) {
-            if (result.specifier === null) {
-              return false;
-            }
+        //   if (result) {
+        //     if (result.specifier === null) {
+        //       return false;
+        //     }
 
-            url = join(M, result.specifier);
-          } else {
-            url = join(M, value);
-          }
-        } else {
-          url = join(M, value);
-        }
+        //     url = join(M, result.specifier);
+        //   } else {
+        //     url = join(M, value);
+        //   }
+        // } else {
+        //   url = join(M, value);
+        // }
 
         const fileResult = await loadAsFile(url);
         if (fileResult) return fileResult;
@@ -53,5 +57,5 @@ export async function loadAsDirectory(
   }
 
   // 2. LOAD_INDEX(X)
-  return loadIndex(M);
+  return loadIndex(packageURL);
 }
---
+++ src/cjs/load_as_directory_test.ts
@@ -0,0 +1,89 @@
+import { loadAsDirectory } from "./load_as_directory.ts";
+import { describe, expect, it } from "../../dev_deps.ts";
+import {
+  emptyPjson,
+  noPjson,
+  pjsonMain,
+  pjsonMain2,
+  pjsonMainModule,
+  pjsonNoMain,
+} from "../../tests/fixtures/node_modules.ts";
+
+describe("loadAsDirectory", () => {
+  it("should resolve as index if the package.json does not exist and index.js exist", async () => {
+    await expect(loadAsDirectory(noPjson.packageURL, { mainFields: [] }))
+      .resolves.toEqual({
+        url: new URL(noPjson.indexJs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve as index if the package.json does not includes any `mainFields`", async () => {
+    await expect(loadAsDirectory(emptyPjson.packageURL, { mainFields: [] }))
+      .resolves.toEqual({
+        url: new URL(emptyPjson.indexJs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve as index if the package.json does not have `mainFields`", async () => {
+    await expect(
+      loadAsDirectory(emptyPjson.packageURL, { mainFields: ["unknown"] }),
+    )
+      .resolves.toEqual({
+        url: new URL(emptyPjson.indexJs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve as file if the package.json includes `mainFields`", async () => {
+    await expect(
+      loadAsDirectory(pjsonMain.packageURL, { mainFields: ["main"] }),
+    )
+      .resolves.toEqual({
+        url: new URL(pjsonMain.mainJs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve as directory if the package.json includes `mainFields`", async () => {
+    await expect(
+      loadAsDirectory(pjsonMain2.packageURL, { mainFields: ["main"] }),
+    )
+      .resolves.toEqual({
+        url: new URL(pjsonMain2.indexJs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve order by mainFields - main", async () => {
+    await expect(
+      loadAsDirectory(pjsonMainModule.packageURL, {
+        mainFields: ["main", "module"],
+      }),
+    )
+      .resolves.toEqual({
+        url: new URL(pjsonMainModule.indexCjs),
+        format: "commonjs",
+      });
+  });
+
+  it("should resolve order by mainFields - module", async () => {
+    await expect(
+      loadAsDirectory(pjsonMainModule.packageURL, {
+        mainFields: ["module", "main"],
+      }),
+    )
+      .resolves.toEqual({
+        url: new URL(pjsonMainModule.indexMjs),
+        format: "module",
+      });
+  });
+
+  it("should throw error if `mainFields` cannot resolve", async () => {
+    await expect(
+      loadAsDirectory(pjsonNoMain.packageURL, { mainFields: ["main"] }),
+    )
+      .rejects.toThrow();
+  });
+});
