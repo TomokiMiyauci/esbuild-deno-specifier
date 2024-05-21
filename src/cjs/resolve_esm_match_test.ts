482fe597fe7d930376aba6fa5dd75702c011b125
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-05-22 00:27:06 +0900

    refactor: remove unused

--- src/cjs/resolve_esm_match_test.ts
+++ src/cjs/resolve_esm_match_test.ts
@@ -1,15 +1,13 @@
 import { resolveEsmMatch } from "./resolve_esm_match.ts";
 import { describe, expect, it } from "../../dev_deps.ts";
 import { noPjson } from "../../tests/fixtures/node_modules.ts";
-import { existFile, readFile, root } from "../../tests/context.ts";
+import { existFile } from "../../tests/context.ts";
 
 describe("resolveEsmMatch", () => {
   it("should throw error if the url does not exist", async () => {
     await expect(
       resolveEsmMatch(import.meta.resolve("./not-found"), {
         existFile,
-        readFile,
-        root,
         specifier: "",
       }),
     ).rejects.toThrow();
@@ -18,8 +16,6 @@ describe("resolveEsmMatch", () => {
   it("should return result", async () => {
     await expect(resolveEsmMatch(noPjson.indexJs, {
       existFile,
-      readFile,
-      root,
       specifier: "",
     })).resolves.toEqual(noPjson.indexJs);
   });
