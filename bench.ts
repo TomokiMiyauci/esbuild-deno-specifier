d6f1e4508fedf15b75fa520c27ec5af89afbb191
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-05-24 16:52:45 +0900

    chore(bench): add bench

---
+++ bench.ts
@@ -0,0 +1,27 @@
+import { denoSpecifier } from "./mod.ts";
+import { denoSpecifier as denoSpecifierBeta } from "jsr:@miyauci/esbuild-deno-specifier@1.0.0-beta.3";
+import { build, BuildOptions } from "npm:esbuild@^0.21.3";
+
+const plugin = denoSpecifier();
+const betaPlugin = denoSpecifierBeta();
+const options = {
+  entryPoints: ["npm:react@^18"],
+  write: false,
+  format: "esm",
+  bundle: true,
+} satisfies BuildOptions;
+const GROUP = "build";
+
+Deno.bench("@latest ", { baseline: true, group: GROUP }, async () => {
+  await build({
+    ...options,
+    plugins: [plugin],
+  });
+});
+
+Deno.bench("prev", { group: GROUP }, async () => {
+  await build({
+    ...options,
+    plugins: [betaPlugin],
+  });
+});
